01355d10aac9541f40c2b6b82194ce293027ae3c
Sunil Kappal <skappal7@users.noreply.github.com>
2021-07-23 23:16:57 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
 mkdir -p ~/.streamlit/
 echo "\
 [general]\n\
-email = \"your-email@domain.com\ "\n\
-" > ~/ .streamlit/credentials.toml
+email = \"your-email@domain.com\"\n\
+" > ~/.streamlit/credentials.toml
 echo "\
 [server]\n\
 headless = true\n\
-enablesCORS=false\n\
+enableCORS=false\n\
 port = $PORT\n\
 " > ~/.streamlit/config.toml
